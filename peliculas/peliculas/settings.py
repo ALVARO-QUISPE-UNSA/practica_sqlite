1a31eb2b4f06b28d259177782b33f3f09f1ec1b2
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-23 21:19:23 -0500

    Se modificó la configuración de la base de datos

--- peliculas/peliculas/settings.py
+++ peliculas/peliculas/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'peliculas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'imdb.db',
     }
 }
 
