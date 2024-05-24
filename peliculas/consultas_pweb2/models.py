106ff48c2abf30a8d766e87676b8f60ad9226e9a
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-23 22:44:42 -0500

    Se añadió los campos del id y del título, se pueden mostrar a través de un /peliculas

--- peliculas/consultas_pweb2/models.py
+++ peliculas/consultas_pweb2/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.db.models import manager
 
 class Movie(models.Model):
-    movie_id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=70)
-    year = models.IntegerField()
-    score = models.FloatField()
-    votes = models.IntegerField()
+    movie_id = models.IntegerField(db_column='MovieID', primary_key=True)
+    title = models.CharField(max_length=70, db_column='Title')
+    #year = models.IntegerField()
+    #score = models.FloatField()
+    #votes = models.IntegerField()
     class Meta:
         managed = False
         db_table = 'Movie'
--- peliculas/consultas_pweb2/views.py
+++ peliculas/consultas_pweb2/views.py
@@ -5,11 +5,13 @@ from django.http import HttpResponse
 def lista_peliculas (req):
     movies = Movie.objects.all()
     first_id = ''
+    titulo = ''
     if movies.exists():
         first_id = movies.first().movie_id
+        titulo = movies.first().title
     #return render(req, '')
-    msg = f"<p>El primer id es: {first_id}</p>"
-    return HttpResponse(msg.encode('utf-8'))
+    msg = f"<p>El primer id es: {first_id} y el titulo: {titulo}</p>"
+    return HttpResponse(msg.encode('utf-8'), content_type='text/html')
 
 
 # Create your views here.
