c60b57d8ac4debcaa68fc826b112480c30df4635
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-23 22:31:49 -0500

    Se trató de añadir la columna de id, pero dice no se contró dicha columna

--- peliculas/consultas_pweb2/views.py
+++ peliculas/consultas_pweb2/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .models import Movie
+from django.http import HttpResponse
 
 def lista_peliculas (req):
     movies = Movie.objects.all()
-    return render(req, 'peliculas.html', {'movies': movies})
+    first_id = ''
+    if movies.exists():
+        first_id = movies.first().movie_id
+    #return render(req, '')
+    msg = f"<p>El primer id es: {first_id}</p>"
+    return HttpResponse(msg.encode('utf-8'))
+
 
 # Create your views here.
