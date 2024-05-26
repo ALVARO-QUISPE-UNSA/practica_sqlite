5ff3a1d71a951698ff1f0eeb683bc339ceaf90f9
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-25 22:02:01 -0500

    Se añadió método para poder cargar los datos desde django

--- peliculas/consultas_pweb2/static/consultas_pweb2/js/peticiones.js
+++ peliculas/consultas_pweb2/static/consultas_pweb2/js/peticiones.js
@@ -1,1 +1,21 @@
 console.log('Hola mundo');
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('opciones').addEventListener('change', function() {
+    let seleccionado = this.value;
+    console.log(seleccionado);
+    let get = 'http://localhost:8000/consultas?tabla=';
+    fetch(get + this.value) 
+      .then( res => {
+        if (!res.ok) 
+          throw new Error('Problema al obtener datos');
+        return res.json();
+      } )
+      .then ( datos => {
+        console.log(datos);
+      } )
+      .catch(err => {
+        console.log('Capturé:\n', err);
+      });
+
+  });
+});
