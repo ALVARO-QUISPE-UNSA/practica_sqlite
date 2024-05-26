b732d34ab12f08a8ac101d1a60f61ec930802d4b
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-26 12:56:01 -0500

    Se añadió script para inicialización

---
+++ iniciar.sh
@@ -0,0 +1,6 @@
+source bin/activate
+cd peliculas/
+python manage.py runserver
+echo "en el buscador, ingresar a: http://localhost:8000/"
+echo "Documento de explicación: https://drive.google.com/open?id=161P63cHeII1AbDA0agc5uEo0OrmYNZlfBA7FE180TSo&usp=drive_copy"
+echo "Vídeo: https://drive.google.com/file/d/18y674BYt1fDlhRNDYeCozHwLVEstbBc6/view?usp=sharing"
