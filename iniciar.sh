4c71e3d2fbadf15138d2be18b3fb01966a1ff9b6
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-26 13:00:05 -0500

    Se añadió correctamente los mensajes en el script

--- iniciar.sh
+++ iniciar.sh
@@ -1,6 +1,7 @@
-source bin/activate
-cd peliculas/
-python manage.py runserver
 echo "en el buscador, ingresar a: http://localhost:8000/"
 echo "Documento de explicación: https://drive.google.com/open?id=161P63cHeII1AbDA0agc5uEo0OrmYNZlfBA7FE180TSo&usp=drive_copy"
 echo "Vídeo: https://drive.google.com/file/d/18y674BYt1fDlhRNDYeCozHwLVEstbBc6/view?usp=sharing"
+
+source bin/activate
+cd peliculas/
+python manage.py runserver
